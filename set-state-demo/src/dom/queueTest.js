b158fbc71b26a557bb5007af2d5caa035b160e91
skliu <skliu@ctrip.com>
2020-01-23 21:33:02 +0800

    feat: update set state demo

--- set-state-demo/src/dom/queueTest.js
+++ set-state-demo/src/dom/queueTest.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 export default class QueueTest extends React.Component {
     constructor(props) {
         super(props);
+        this.refresh = 0;
         this.state = {
             queueCount: 0,
             anotherCount: 0
@@ -20,11 +21,11 @@ export default class QueueTest extends React.Component {
     }
 
     render() {
+        console.log(`refresh count: ${++this.refresh}`);
+        console.log(`queueCount: ${this.state.queueCount}, anotherCount: ${this.state.anotherCount}`);
         return (
             <div id={'sync'} style={{ width: 400, height: 100, background: '#0193' }}>
-                {`queueCount: ${this.state.queueCount}`}
-                <br/>
-                {`anotherCount: ${this.state.anotherCount}`}
+                queue test
             </div>
         );
     }
